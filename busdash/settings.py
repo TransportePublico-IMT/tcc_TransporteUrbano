79aaeffb0fe3b5e2f8204b4a20d3a61fe9d9d95a
Luca Ezellner Miraglia <luca.miraglia@uol.com.br>
2020-07-23 19:17:37 -0300

    Adiciona API da aplicação
    Utilização do django_restframework para criação da api para dados dos onibus

--- busdash/settings.py
+++ busdash/settings.py
@@ -44,7 +44,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "panel",
+    "onibus",
     "django_plotly_dash.apps.DjangoPlotlyDashConfig",
 ]
 
--- panel/models.py
+++ panel/models.py
@@ -1,12 +1,1 @@
 from django.db import models
-
-
-class Dashboard(models.Model):
-    last_update = models.DateTimeField()
-
-
-class Chart(models.Model):
-    title = models.CharField(max_length=255)
-    chart_type = models.CharField(max_length=255)
-    data_json = models.TextField()
-    dashboard = models.ForeignKey(Dashboard, on_delete=models.CASCADE)
