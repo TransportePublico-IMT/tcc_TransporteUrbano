abc1db24f5c4f1df8be6cc0def71d38ee2b4ac4c
Lucas Marques <lucas_lutinho@hotmail.com>
2020-08-05 19:07:25 -0300

    Inclusão do post da posição dos onibus

--- busdash/tasks.py
+++ busdash/tasks.py
@@ -11,3 +11,8 @@ def create_paradas_if_not_exist(self):
 def create_linhas_if_not_exist(self):
     status_json = popular_db_sp_trans.popular_linhas()
     return status_json['status']
+
+@shared_task(bind=True)
+def create_onibus(self):
+    status_json = popular_db_sp_trans.popular_onibus()
+    return status_json['status']
