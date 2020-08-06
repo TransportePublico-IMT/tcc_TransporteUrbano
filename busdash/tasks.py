83db830eb69e8b4f5c861c26789af2e49a72e185
Luca Ezellner Miraglia <luca.miraglia@uol.com.br>
2020-08-05 22:28:20 -0300

    Adiciona tasks periódicas para salvamento de linhas, onibus e paradas 👏👏

--- busdash/tasks.py
+++ busdash/tasks.py
@@ -1,18 +1,21 @@
-from celery import shared_task
+from celery.task.schedules import crontab
+from celery.decorators import periodic_task
 from helpers import popular_db_sp_trans
 import time
 
-@shared_task(bind=True)
-def create_paradas_if_not_exist(self):
+#celery -A busdash worker --pool=gevent -l info
+
+@periodic_task(run_every=(crontab(minute='*/1')), name="create_paradas_if_not_exist", ignore_result=True)
+def create_paradas_if_not_exist():
     status_json = popular_db_sp_trans.popular_paradas()
     return status_json['status']
 
-@shared_task(bind=True)
-def create_linhas_if_not_exist(self):
+@periodic_task(run_every=(crontab(minute='*/1')), name="create_linhas_if_not_exist", ignore_result=True)
+def create_linhas_if_not_exist():
     status_json = popular_db_sp_trans.popular_linhas()
     return status_json['status']
 
-@shared_task(bind=True)
-def create_onibus(self):
+@periodic_task(run_every=(crontab(minute='*/1')), name="save_onibus_posicao", ignore_result=True)
+def save_onibus_posicao():
     status_json = popular_db_sp_trans.popular_onibus()
     return status_json['status']
