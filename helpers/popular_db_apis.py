11ad7dc02fb9f5f58d2a61babd784f338f07729a
Lucas Marques <lucas_lutinho@hotmail.com>
2020-08-10 19:41:53 -0300

    Inicio da inclusão da api do ClimaTempo

--- helpers/popular_db_apis.py
+++ helpers/popular_db_apis.py
@@ -35,3 +35,34 @@ def popular_trens_metros():
     r = requests.post(url, data=json.dumps(data), headers=headers)
 
     return r.json()
+
+def popular_climatempo():
+    idCity_STAndre = 3667
+    token = "cd640a1a7fd7767e9afc268efcb06882"
+    url = "http://apiadvisor.climatempo.com.br/api/v1/weather/locale/" + str(idCity_STAndre) + "/current?token=" + token
+    data = api_get_data(url)
+    list_clima = []
+    i = data["data"]
+    clima = {
+        'id_cidade': data["id"],
+        'temperatura': i['temperature'],
+        'direcao_vento': i["wind_direction"],
+        'velocidade_vento': i["wind_velocity"],
+        'umidade': i["humidity"],
+        'condicao': i["condition"],
+        'pressao': i["pressure"],
+        #'icone': i["icon"], #????? vamos colocar?
+        'sensacao': i["sensation"],
+        'date': i["date"]
+    }
+    list_clima.append(clima)
+    url = "http://localhost:8000/api/climatempo/"
+    headers = {
+        'Content-Type': 'application/json',
+        'Connection' : 'keep-alive',
+        'Accept': "*/*"
+    }
+    data = {'ct': list_clima}
+    r = requests.post(url, data=json.dumps(data), headers=headers)
+
+    return r.json()
--- trem/models.py
+++ trem/models.py
@@ -9,3 +9,18 @@ class Trem(models.Model):
 
     def __str__(self):
         return str(self.id_linha)
+
+# class ClimaTempo(models.Model):
+#     id_cidade = models.IntegerField()
+#     temperatura = models.DecimalField(max_digits=5, decimal_places=2)
+#     direcao_vento = models.CharField(max_length=10)
+#     velocidade_vento = models.DecimalField(max_digits=5, decimal_places=2)
+#     umidade = models.DecimalField(max_digits=5, decimal_places=2)
+#     condicao = models.CharField(max_length=500)
+#     pressao = models.DecimalField(max_digits=10, decimal_places=2)
+#     #icone = models.CharField(max_length=500)
+#     sensacao = models.DecimalField(max_digits=5, decimal_places=2)
+#     date = models.DateTimeField()
+
+#     def __str__(self):
+#         return str(self.temperatura)
