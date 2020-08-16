1dff3d836189c84000ac25e7f252b14d5d4e9e0c
Arthur Novello <novelloarthur@gmail.com>
2020-08-16 18:23:03 -0300

    terraform/iam

--- terraform/04_loadbalancer.tf
+++ terraform/04_loadbalancer.tf
@@ -1,39 +1,39 @@
 # Load Balancer
 resource "aws_lb" "production" {
-    name = "${var.ecs_cluster_name}-alb"
-    load_balancer_type = "application"
-    internal = false
-    security_group = [aws_security_group.load-balancer.id]
-    subnets = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
+  name               = "${var.ecs_cluster_name}-alb"
+  load_balancer_type = "application"
+  internal           = false
+  security_groups    = [aws_security_group.load-balancer.id]
+  subnets            = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
 }
 
 # Target Group
 resource "aws_alb_target_group" "default-target-group" {
-    name = "${var.ecs_cluster_name}-tg"
-    port = 80
-    protocol = "HTTP"
-    vpc_id = aws_vpc.production-vpc.id
+  name     = "${var.ecs_cluster_name}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.production-vpc.id
 
-    health_check {
-      healthy_threshold = var.health_check_path
-      port = "traffic-port"
-      healthy_threshold = 5
-      unhealthy_threshold = 2
-      timeout = 2
-      interval = 5
-      matcher = "200"
-    }
+  health_check {
+    path                = var.health_check_path
+    port                = "traffic-port"
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    timeout             = 2
+    interval            = 5
+    matcher             = "200"
+  }
 }
 
 # Listener
 resource "aws_alb_listener" "ecs-alb-http-listener" {
-    load_balancer_arn = aws_lb.production.id
-    port = "80"
-    protocol = "HTTP"
-    depends_on = [aws_alb_target_group.default-target-group]
+  load_balancer_arn = aws_lb.production.id
+  port              = "80"
+  protocol          = "HTTP"
+  depends_on        = [aws_alb_target_group.default-target-group]
 
-    default_action{
-      type = "forward"
-      target_group_arn = aws_alb_target_group.default-target-group.arn
-    }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.default-target-group.arn
+  }
 }
