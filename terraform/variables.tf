4dd706ce441320399946bcfc8fc12ae101b197af
Arthur Novello <novelloarthur@gmail.com>
2020-08-16 17:02:33 -0300

    gitflow-feature-stash: terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,29 @@
+# core
+variable "region" {
+  description = "Regiao da AWS"
+  default     = "us-east-2"
+}
+
+# networking
+
+variable "public_subnet_1_cidr" {
+  description = "CIDR Block for Public Subnet 1"
+  default     = "10.0.1.0/24"
+}
+variable "public_subnet_2_cidr" {
+  description = "CIDR Block for Public Subnet 2"
+  default     = "10.0.2.0/24"
+}
+variable "private_subnet_1_cidr" {
+  description = "CIDR Block for Private Subnet 1"
+  default     = "10.0.3.0/24"
+}
+variable "private_subnet_2_cidr" {
+  description = "CIDR Block for Private Subnet 2"
+  default     = "10.0.4.0/24"
+}
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+  default     = ["us-east-2a", "us-east-2b"]
+}
