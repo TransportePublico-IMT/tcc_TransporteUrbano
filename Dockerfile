8272e5a23c29238cc2520160ade8408d0a17b88c
Arthur Novello <novelloarthur@gmail.com>
2020-08-30 17:10:41 -0300

    terraform/staticfiles

--- Dockerfile
+++ Dockerfile
@@ -20,3 +20,6 @@ RUN pip install -r requirements.txt
 
 # copy project
 COPY . .
+
+# collect static files
+RUN python manage.py collectstatic --no-input
--- busdash/settings.py
+++ busdash/settings.py
@@ -173,10 +173,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "busdash/static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "busdash/staticfiles")]
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "/staticfiles/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 CELERY_BROKER_URL = "redis://localhost:6379"
 CELERY_RESULT_BACKEND = "django-db"
